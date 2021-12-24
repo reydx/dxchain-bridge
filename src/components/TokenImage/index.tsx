40f65d105c5e405bcf21a9a343cd028f74a862ee
wysdx <wangyongsong@dxchain.onaliyun.com>
2021-12-24 09:36:35 +0800

    fix: build error

--- src/components/TokenImage/index.tsx
+++ src/components/TokenImage/index.tsx
@@ -2,15 +2,15 @@ import { SerializedToken } from '@/models/useGetState';
 import './index.less';
 
 type Props = {
-  token: SerializedToken;
+  token: any;
 };
-
+// SerializedToken
 export function TokenImage(props: Props) {
   const { token } = props;
   return (
     <div className="token-image">
-      <img src={`./tokens/${token.assetName}.png`} alt="" />
-      {token.assetName}
+      <img src={`./tokens/${token?.assetName}.png`} alt="" />
+      {token?.assetName}
     </div>
   );
 }
